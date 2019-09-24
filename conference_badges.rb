13828fd9a7141d485b06804928261dff528812e0
Darrian Soto <ddog350@gmail.com>
2019-09-24 21:48:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,7 @@ def batch_badge_creator(attendees)
   new_array = []
   attendees.each do |name|
     new_array.push(badge_maker(name))
+    new_array.push("Hello, my name is #{name}."
   end
   new_array
 end
